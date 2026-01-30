278f4fdd4334cc3fd2052109b8e611d3d140c4ea
Rainhard55 <95255589+Rainhard55@users.noreply.github.com>
2026-01-30 10:55:02 -0800

    Add files via upload

--- CSP_5_01_Searching.py
+++ CSP_5_01_Searching.py
@@ -1,11 +1,28 @@
-
+import random
 def randomSearch(items:list, target) -> int:
-    #Modify the below function such that it takes in a list of items and a target value.
-    #Randomly choose an item from the list and if it isn't the target value try again.
-    #print out the amount of tries it took and return the index of the target value
-    pass
+    tries = 0
+
+    while True:
+        index = random.randint(0, len(items)-1)
+        tries += 1
+
+        if items[index] == target:
+            print(f"Found {target} at index {index}")
+            return index
+pass
+print(randomSearch([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], 10))
 
 def linearSearch(items:list, target) ->tuple[int,int]:
+    checks = 0
+
+    for index in range(len(items)):
+        checks += 1
+        if items[index] == target:
+            return index, checks
+
+    return -1, checks
+
+
     #Modify the below function such that it implements linear search.
     #Return the index of the target value and the amount of checks it took
     #if the value is not within the list return -1 as the index.
@@ -13,7 +30,21 @@ def linearSearch(items:list, target) ->tuple[int,int]:
 
 
 def binarySearch(items:list, target) -> tuple[int,int]:
+    left, right = 0, len(items)-1
+    checks = 0
+    while left <= right:
+        mid = (left+right)//2
+        checks += 1
+        if items[mid] == target:
+            return mid, checks
+        elif items[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1, checks
     # Modify the below function such that it implements linear search.
     # Return the index of the target value and the amount of checks it took
     # if the value is not within the list return -1 as the index.
-    pass
+
+pass
